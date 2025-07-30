adf38e288381edfac9c4b1803f34a9fd1303b250
rowbie <rowbieworking@gmail.com>
2025-07-30 23:52:30 +0300

    Update web folder

--- web/js/createOptions.js
+++ web/js/createOptions.js
@@ -16,7 +16,7 @@ export function createOptions(type, data, id, zoneId) {
 
   const option = document.createElement("div");
   const iconElement = `<i class="fa-fw ${data.icon} option-icon" ${
-    data.iconColor ? `style = color:${data.iconColor} !important` : null
+    data.iconColor ? `style = color:${data.iconColor} !important` : '#f8f9fa'
   }"></i>`;
 
   option.innerHTML = `${iconElement}<p class="option-label">${data.label}</p>`;
